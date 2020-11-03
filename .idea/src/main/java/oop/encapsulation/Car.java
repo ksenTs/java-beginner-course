7445dffa58ed233101a6c9750a7c010e7f5606c6
ksenTs <140498grr>
2020-11-04 01:58:28 +0300

    Encapsulation

---
+++ .idea/src/main/java/oop/encapsulation/Car.java
@@ -0,0 +1,27 @@
+package oop.encapsulation;
+
+public class Car {
+    //first example
+    private String model;
+    private String number;
+
+    public Car(String model, String number) {
+        this.model = model;
+        this.number = number;
+    }
+
+    // second example
+    private void swicthSpeed() {
+        // swicth logic
+    }
+
+    public void run() {
+        // run logic
+    }
+
+    public void stop() {
+        // stop logic
+    }
+
+
+}
